946deb8b3221fc63a3f22413fb800ffb7e1ec7cd
syedbasit <syedbasitali2015@hotmail.com>
2015-05-05 17:35:39 +0500

    Product Autocomplete implemented for Description

--- IMS/Result.aspx.cs
+++ IMS/Result.aspx.cs
@@ -35,7 +35,7 @@ namespace IMS
             }
             if (clientName.Length >= 2)
             {
-                using (SqlCommand cmd = new SqlCommand("select DISTINCT Product_Name from tbl_ProductMaster where Product_Name LIKE  @Client_Name+'%'", connection))
+                using (SqlCommand cmd = new SqlCommand("select DISTINCT Product_Name from tbl_ProductMaster where Description LIKE  @Client_Name+'%'", connection))
                 {
 
                     SqlParameter parClientName = new SqlParameter("@Client_Name", SqlDbType.VarChar);
@@ -58,6 +58,33 @@ namespace IMS
 
                     Response.Write(sb.ToString());
                 }
+                if (dt.Rows.Count <= 0)
+                {
+                    using (SqlCommand cmd = new SqlCommand("select DISTINCT Product_Name from tbl_ProductMaster where Product_Name LIKE  @Client_Name+'%'", connection))
+                    {
+
+                        SqlParameter parClientName = new SqlParameter("@Client_Name", SqlDbType.VarChar);
+                        parClientName.Value = clientName;
+                        cmd.Parameters.Add(parClientName);
+                        //SqlDataReader dr = cmd.ExecuteReader();
+
+                        SqlDataAdapter da = new SqlDataAdapter(cmd);
+                        da.Fill(dt);
+                        StringBuilder sb = new StringBuilder();
+
+                        if (dt.Rows.Count > 1)
+                        {
+                            for (int i = 0; i < dt.Rows.Count; i++)
+                            {
+                                sb.Append(dt.Rows[i].ItemArray[0].ToString() + "~");   //Create Con
+
+                            }
+                        }
+
+                        Response.Write(sb.ToString());
+                    }
+                }
+                
             }
             
              
