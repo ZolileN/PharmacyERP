1111776ece497f4d948d750119feca12977a0050
MoizRauf <moiz.rauf@hotmail.com>
2015-06-17 09:08:19 +0500

    partial changes Item request to warehouse
    These changes are hidden in site master, im just adding the pages for
    deployment

---
+++ IMS/SelectWarehouse.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace IMS
+{
+    public partial class SelectWarehouse : System.Web.UI.Page
+    {
+        public static SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["IMSConnectionString"].ToString());
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) 
+            {
+                #region Populating Warehouse
+                try
+                {
+                    DataSet dsS = new DataSet();
+                    if (connection.State == ConnectionState.Closed)
+                    {
+                        connection.Open();
+
+                    }
+                    SqlCommand command = new SqlCommand("Sp_GetSystems_ByRoles", connection);
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    command.Parameters.AddWithValue("@p_RoleName", "Warehouse");
+                    command.Parameters.AddWithValue("@p_systemName", DBNull.Value);
+                    SqlDataAdapter sA = new SqlDataAdapter(command);
+                    sA.Fill(dsS);
+                    ddlWH.DataSource = dsS.Tables[0];
+                    ddlWH.DataTextField = "SystemName";
+                    ddlWH.DataValueField = "SystemID";
+                    ddlWH.DataBind();
+                    if (ddlWH != null)
+                    {
+                        ddlWH.Items.Insert(0, "Select Warehouse");
+                        ddlWH.SelectedIndex = 0;
+                    }
+                   
+                }
+                catch (Exception ex)
+                {
+
+                }
+                finally
+                {
+                    if (connection.State == ConnectionState.Open)
+                    {
+                        connection.Close();
+                    }
+                }
+                #endregion
+            }
+        }
+
+        protected void btnContinue_Click(object sender, EventArgs e)
+        {
+            if (ddlWH.SelectedIndex > 0)
+            {
+                Session["WH_Name"] = ddlWH.SelectedItem.ToString();
+                Session["WH_ID"] = ddlWH.SelectedValue;
+                Response.Redirect("ItemRequestWH.aspx");
+            }
+        }
+    }
+}
